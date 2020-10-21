20ef6e8755868800103b3f627f8e78e65e9b7873
Tulio Gaio <tuliogaio@gmail.com>
2020-10-21 16:51:17 -0300

    Update the Django

--- eventex/settings.py
+++ eventex/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = (
     'eventex.core',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
