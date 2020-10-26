85ea92cb942aa2e91941e7fbca2034c094e31586
Tulio Gaio <tuliogaio@gmail.com>
2020-10-25 22:48:52 -0300

    Alterando configurações de e-mail

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
-
 from eventex.subscriptions.forms import SubscriptionForm
 
 
@@ -14,8 +13,8 @@ def subscribe(request):
             body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
             mail.send_mail('Confirmação de Inscrição',
                            body,
-                           'contato@eventex.com.br',
-                           ['contato@eventex.com.br', form.cleaned_data['email']])
+                           'gmvhorasextras@gmail.com',
+                           ['gmvhorasextras@gmail.com', form.cleaned_data['email']])
             messages.success(request, 'Inscrição realizada com sucesso!')
             return HttpResponseRedirect('/inscricao/')
         else:
