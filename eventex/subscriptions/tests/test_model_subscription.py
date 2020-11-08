6034f7bcf72a05c60d17679064a29a372359a402
Tulio Gaio <tuliogaio@gmail.com>
2020-11-08 20:05:09 -0300

    Persistindo a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Túlio Gaio',
+            cpf='12345678901',
+            email="tuliogaio@gmail.com",
+            phone="94992415990"
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -22,6 +23,8 @@ def create(request):
 
     # send email
     _send_mail('Confirmação de Inscrição', settings.DEFAULT_FROM_EMAIL, form.cleaned_data['email'], 'subscriptions/subscription_email.txt', form.cleaned_data)
+
+    Subscription.objects.create(**form.cleaned_data)
 
     # success feedback
     messages.success(request, 'Inscrição realizada com sucesso!')
